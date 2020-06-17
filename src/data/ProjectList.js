b9f396d57015f14cffe9b0bdac832808406fb18d
Jhogne <hogne@student.chalmers.se>
2020-06-17 20:26:02 +0000

    Updated project descriptions and minor refactoring

---
+++ src/data/ProjectList.js
@@ -0,0 +1,28 @@
+const ProjectList = [
+  {
+    title: "Personal webpage",
+    source: "https://github.com/Jhogne/PersonalPage",
+    language: "Javascript",
+    //keywords: 'React, AWS',
+    keywords: '',
+    description: "I created this webpage as a portfolio for my projects, and as a playground to give me real world experience with web development. I built the front end with ReactJS, and it is hosted on AWS EC2 server.",
+    //description: "This is what you are looking at right now. I created this webpage as a portfolio for the projects I have created, as well as learning the basics of for web development. At the moment it is a simple static html/css page running from an Amazon EC2 server. "
+  },
+  {
+    title: "Spotify playlist creator",
+    source: "https://github.com/Jhogne/Spotify-playlists",
+    language: "Python",
+    keywords: '',
+    description: "I found myself lacking the functionality to easily create large playlists in Spotify, so I created a terminal tool to provide this. It uses the Spotify API to let the user create playlists of all songs created by a list of artists."
+  },
+  {
+    title: "Advent of Code",
+    source: "https://github.com/Jhogne/aoc19",
+    language: "Python",
+    keywords: '',
+    description: "In participated in advent of code 2019 for fun and used it as an opportunity to get some hands on experience with Python 3, since I had never actually programmed in Python before."
+  },
+
+];
+
+export default ProjectList
